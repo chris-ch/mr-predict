79fb5d84fa5a3e10da792942b7c4df1b8f6f7f9f
chris-ch <ch.alexandre@bluewin.ch>
2014-02-22 11:17:42 +0100

    improved command-line interface

--- dtl-run.py
+++ dtl-run.py
@@ -10,8 +10,8 @@ import argparse
 
 from predict.decisiontree.forest import RandomForest
 
-def config_logging(level):   
-    # create logger 
+def config_logging(level):
+    # create logger
     logger = logging.getLogger('training')
     level_mapping = {
         'debug': logging.DEBUG,
@@ -19,16 +19,16 @@ def config_logging(level):
         'warn': logging.WARNING,
     }
     logger.setLevel(level_mapping[level])
-    
+
     # create console handler and set level to debug
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
-    
+
     # add ch to logger
     logger.addHandler(ch)
-    
+
 def main(args):
     config_logging(args.log_level)
     forest_file = args.forest
@@ -39,51 +39,77 @@ def main(args):
     samples = csv.reader(args.csv_input_file, delimiter=',')
     first_line = next(samples)
     header = first_line[1:]
-    out = csv.writer(args.output)
-    out.writerow([first_line[0], args.target_column])
-    for sample_data in samples:
-        sample_id = sample_data[0]
-        sample = dict()
-        for index, column in enumerate(sample_data[1:]):
-            try:
-                sample[header[index]] = float(column)
-                
-            except ValueError:
-                pass
-        
-        value = forest.predict(sample)
-        out.writerow([sample_id, value])
-        
-        
-    
+    with open(args.output, 'wb') as output_file:
+        out = csv.writer(output_file)
+        out.writerow([first_line[0], args.target_column])
+        for sample_data in samples:
+            sample_id = sample_data[0]
+            sample = dict()
+            for index, column in enumerate(sample_data[1:]):
+                try:
+                    sample[header[index]] = float(column)
+
+                except ValueError:
+                    pass
+
+            value = forest.predict(sample)
+            out.writerow([sample_id, value])
+
+    if args.check:
+        from itertools import izip
+        with open(args.output, 'rb') as res_file:
+            ref_rows = csv.reader(args.check)
+            res_rows = csv.reader(res_file)
+            next(ref_rows)
+            next(res_rows)
+            errors_count = 0
+            samples_count = 0
+            for ref_row, res_row in izip(ref_rows, res_rows):
+                ref_value = float(ref_row[1])
+                samples_count += 1
+                if res_row[1] != 'None':
+                    res_value = round(float(res_row[1]))
+                    errors_count += (0, 1)[abs(ref_value - res_value) != 0.0]
+
+                else:
+                    errors_count += 1
+
+            print 'cheking results: %d errors detected from a total of %d samples' % (errors_count, samples_count)
+
+
+
 if __name__ == '__main__':
-    
+
     parser = argparse.ArgumentParser(description='Runs a Random Forest on a set',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('csv_input_file',
         type=argparse.FileType('r'),
         help='CSV file (including header) with list of samples. First column serves as sample identifier.')
-    
+
     parser.add_argument('-o', '--output',
-        type=argparse.FileType('w'),
+        type=str,
         default='results.csv',
         help='output of the regression for each sample')
-    
+
     parser.add_argument('-f', '--forest',
         type=argparse.FileType('r'),
         default='forest',
         help='forest data obtained from training phase')
-    
+
+    parser.add_argument('-c', '--check',
+        type=argparse.FileType('r'),
+        help='file used for cross-checking the results')
+
     parser.add_argument('-t', '--target-column',
         type=str,
         default='target',
         help='name of the ouput column')
-    
+
     parser.add_argument('-l', '--log-level',
         type=str,
         default='info',
         choices=['debug', 'info', 'warn'],
         help='sets the level for logging messsages')
-    
+
     args = parser.parse_args()
     main(args)
